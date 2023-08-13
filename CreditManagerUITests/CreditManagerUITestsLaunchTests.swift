f3fd0229442a5b5dc54372416e18d437ba02215b
ritik <ritiksharma2502@gmail.com>
2023-08-13 14:24:48 +0530

    Initial Commit

---
+++ CreditManagerUITests/CreditManagerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CreditManagerUITestsLaunchTests.swift
+//  CreditManagerUITests
+//
+//  Created by Ritik Sharma on 13/08/23.
+//
+
+import XCTest
+
+final class CreditManagerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
